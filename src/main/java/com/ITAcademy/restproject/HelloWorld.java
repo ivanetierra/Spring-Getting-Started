f552896db0154233ea2bbe71caca2012ac165137
Ivan <ielizalde@gmail.com>
2020-07-14 10:38:58 +0200

    second commit

--- src/main/java/com/ITAcademy/restproject/HelloWorld.java
+++ src/main/java/com/ITAcademy/restproject/HelloWorld.java
@@ -1,25 +1,26 @@
 package com.ITAcademy.restproject;
 
-import org.springframework.web.bind.annotation.*;
-
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-public class HelloWorld {
+class HelloWorld {
 
     @RequestMapping("/")
     public String index() {
         return "Hello World";
     }
 
-    private static final String template = "Hello, ";
+    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greetings")
-    public Greetings greetings(@RequestParam(value = "Ivan", defaultValue = "World") final String name) {
-
-        return new Greetings(counter.incrementAndGet(), String.format(template, name));
+    @GetMapping("/greeting") //http://localhost:8080/greeting?name=User
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
-
 
